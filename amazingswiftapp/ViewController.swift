7cfc61230872c8e96fe7a6c5c719909b0a265511
SidBala <Sid.Bala@outlook.com>
2016-06-23 13:12:10 -0700

    Add async md set

--- amazingswiftapp/ViewController.swift
+++ amazingswiftapp/ViewController.swift
@@ -15,6 +15,15 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
     
+    @IBAction func setAsync(sender: AnyObject) {
+        let delayInSeconds = 1.0
+        let popTime = dispatch_time(DISPATCH_TIME_NOW,
+                                    Int64(delayInSeconds * Double(NSEC_PER_SEC))) // 1
+        dispatch_after(popTime, dispatch_get_main_queue()) { // 2
+            
+            BuddyBuildSDK.setCrashMetadataObject("AsyncValue", forKey: "AsyncKey");
+        }
+    }
     @IBAction func setNew(sender: AnyObject) {
         BuddyBuildSDK.setCrashMetadataObject("576af2a10077a7dd", forKey: "UserID");
         BuddyBuildSDK.setCrashMetadataObject("Login Page", forKey: "CurrentState");
