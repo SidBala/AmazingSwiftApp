39b5a978667676896ef9675ec29157be8b02e778
SidBala <Sid.Bala@outlook.com>
2016-09-20 19:40:40 -0700

    Fail a particular test

--- amazingswiftappTests/amazingswiftappTests.swift
+++ amazingswiftappTests/amazingswiftappTests.swift
@@ -26,6 +26,12 @@ class amazingswiftappTests: XCTestCase {
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
     
+    func testFail() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssert(false)
+    }
+    
     func testPerformanceExample() {
         // This is an example of a performance test case.
         self.measureBlock {
